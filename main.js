7eaf69b999a4a472121ec2631b622d9de6d70a1d
eumtsspfc <mateus.ramos.martins@escola.pr.gov.br>
2026-06-11 10:43:14 +0000

    Atualiza arquivos do calendário

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const botoes = document.querySelectorAll(".botao");
-const abas = document.querySelectorAll(".aba-conteudo");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
     for (let j = 0; j < botoes.length; j++) {
       botoes[j].classList.remove("ativo");
-      abas[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
     }
 
     botoes[i].classList.add("ativo");
-    abas[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
   };
 }
